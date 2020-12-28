b9b498679c99d4dfb98f227867451d88c47df61a
rahiyansafz <rahiyansafin@gmail.com>
2020-12-28 11:59:17 +0600

    Initial commit

---
+++ app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule }            from '@angular/core';
+import { BrowserModule }       from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';  // <-- #1 import module
+
+import { AppComponent }        from './app.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // <-- #1 import component
+import { HeroListComponent }   from './hero-list/hero-list.component';
+
+import { HeroService }         from './hero.service'; //  <-- #1 import service
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule // <-- #2 add to @NgModule imports
+  ],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent,
+    HeroListComponent
+  ],
+  exports: [ // export for the DemoModule
+    AppComponent,
+    HeroDetailComponent,
+    HeroListComponent
+  ],
+  providers: [ HeroService ], // <-- #4 provide HeroService
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
+
+
+/*
+Copyright 2017-2018 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
